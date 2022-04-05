f2c070fd06f789555347cca173348f80e05f602d
E. M. Greeff <unmodster@gmail.com>
2022-04-05 08:23:57 -0600

    just stars

--- src/App.js
+++ src/App.js
@@ -15,12 +15,12 @@ function Stars(props) {
   const ref = useRef()
   const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.5 }))
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10
-    ref.current.rotation.y -= delta / 15
+    ref.current.rotation.x += delta / 1500
+    ref.current.rotation.y += delta / 1500
   })
   return (
-    <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props}>
+    <group rotation={[0, 0, Math.PI / 10]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled={true} {...props}>
         <PointMaterial transparent color="#ffa0e0" size={0.005} sizeAttenuation={true} depthWrite={false} />
       </Points>
     </group>
