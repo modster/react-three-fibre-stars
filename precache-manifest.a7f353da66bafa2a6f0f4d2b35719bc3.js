fce15e659210ef43a189bf2a5cd86064eff38f9a
E. M. Greeff <unmodster@gmail.com>
2022-04-09 00:41:55 -0600

    Updates

---
+++ precache-manifest.a7f353da66bafa2a6f0f4d2b35719bc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "527e57ec383e59eff39e",
+    "url": "./static/css/main.3c3381cc.chunk.css"
+  },
+  {
+    "revision": "527e57ec383e59eff39e",
+    "url": "./static/js/main.4c215513.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "833308c724f534627964",
+    "url": "./static/js/2.ee1b7c16.chunk.js"
+  },
+  {
+    "revision": "9cc4050191a6abd863e1bee59989e72a",
+    "url": "./index.html"
+  }
+];
