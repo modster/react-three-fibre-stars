3815766616a0936c451e003ea59b9e8cd42c70b1
E. M. Greeff <unmodster@gmail.com>
2022-04-18 00:06:46 -0600

    looks good, merge

--- src/App.js
+++ src/App.js
@@ -2,20 +2,36 @@ import { EffectComposer, DepthOfField, Bloom, Noise, Vignette, BrightnessContras
 import { BlurPass, Resizer, KernelSize } from 'postprocessing'
 import { Canvas } from '@react-three/fiber'
 import Stars from './components/StarScene/StarScene'
+import { OrbitControls } from '@react-three/drei'
+// import { Text } from "troika-three-text";
+// import fonts from "./components/Text/fonts";
+
+// extend({ Text });
+// const text = "GREEFFER";
 
 export default function App() {
+
   return (
     <Canvas camera={{ fov: 80, position: [0, 0, 1] }}>
       {/* Your regular scene contents go here, like always ... */}
-      <Stars />
-      <EffectComposer>
-        <ambientLight intensity={0.1} />
-        {/* <directionalLight color="blue" position={[0, 0, 5]} /> */}
+      <OrbitControls enableZoom={true} enablePan={true} enableRotate={false} />
 
-        {/* <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} /> */}
+      <ambientLight />
+
+      <Stars />
+
+      <pointLight position={[-100, 0, -160]} />
+      <pointLight position={[0, 0, -170]} />
+      <pointLight position={[100, 0, -160]} />
+      <EffectComposer multisampling={8}>
+
+        {/* <directionalLight color="blue" position={[0, 0, 5]} /> */}
+        <Bloom kernelSize={3} luminanceThreshold={0} luminanceSmoothing={0.2} intensity={0.3} />
+        <Bloom kernelSize={KernelSize.HUGE} luminanceThreshold={0} luminanceSmoothing={0} intensity={0.5} />
+        <DepthOfField focusDistance={0.00} focalLength={0.02} bokehScale={2} />
         {/* <Bloom intensity={0} luminanceThreshold={0} luminanceSmoothing={1} /> */}
         {/* <Noise opacity={0.02} /> */}
-        <Vignette eskil={true} offset={0.6} darkness={0.6} />
+        <Vignette eskil={true} offset={0.4} darkness={0.4} />
       </EffectComposer>
     </Canvas>
   )
--- src/index.js
+++ src/index.js
@@ -2,19 +2,31 @@ import ReactDOM from 'react-dom'
 import './styles.css'
 import App from './App'
 
+function Overlay() {
+  return (
+    <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
+      <div style={{ position: 'absolute', top: '0', left: '0' }}>
+        <h1 style={{ margin: 20, padding: 0, fontSize: '2em', fontWeight: 500, letterSpacing: '-0.05em' }}>Greeffer.com</h1>
+
+        <div>
+          <ul>
+            <li>Github</li>
+
+
+
+
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <>
     <App />
+    <Overlay />
   </>,
   document.getElementById('root')
+
 )
-// function Overlay() {
-//   return (
-//     <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
-//       <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate3d(-50%,-50%,0)' }}>
-//         <h1 style={{ margin: 0, padding: 0, fontSize: '15em', fontWeight: 500, letterSpacing: '-0.05em' }}>hello</h1>
-//       </div>
-//     </div>
-//   )
-// }<Overlay />
